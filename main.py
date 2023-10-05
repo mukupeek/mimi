d87126af8491d7b27652499229030b304fe08337
mukupeek <fd.fd.com@yandex.ru>
2023-10-06 00:37:50 +0300

    (Denis

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-import os
-
 import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+# @bot.command()
+# async def helper(ctx):
+#  embed = discord.Embed(title="Список команд", description="Список доступных команд бота", color=discord.Color.blue())
+#
+#     embed.add_field(name="!post", value="Создаёт пост от имени бота", inline=False)
+#     embed.add_field(name="!ping", value="Пинг Понг", inline=False)
+#
+#     await ctx.send(embed=embed)
+
+
+@bot.event
+async def on_member_remove(member):
+    message = '***Пока пока*** :( ' + member.mention
+    channel = bot.get_channel(1038151796180402257)
+    await channel.send(message)
 
 
 @bot.event
@@ -50,13 +65,17 @@ async def on_raw_reaction_add(payload):
                 await payload.member.add_roles(discord.utils.get(guild.roles, id=1038546466278481940))
 
     async def verif():
+        member = payload.member
         if payload.emoji.name == '✅':
             num = [1038199925189640323, 1037845308517924885, 1037845381188435969, 1038199922379464776,
                    1038213629318799360,
                    1038464914718724248]
             for draw in range(len(num)):
                 await payload.member.add_roles(discord.utils.get(guild.roles, id=num[draw]))
-                await reaction.remove(user=payload.member)
+            await reaction.remove(user=payload.member)
+            message_to_hi = '***Привет привет, милый котик :3*** ' + member.mention
+            channel_to_hi = bot.get_channel(1038151796180402257)
+            await channel_to_hi.send(message_to_hi)
 
     match payload.message_id:
         case 1038519665040838677:
@@ -103,8 +122,6 @@ async def on_raw_reaction_remove(payload):
                 await payload.member.remove_roles(discord.utils.get(guild.roles, id=1038545555799937084))
 
     match payload.message_id:
-        # case 1038519665040838677:
-        #     await verif()
         case 1038579472800153610:
             await boys_and_girls()
         case 1038576815456931900:
@@ -119,7 +136,7 @@ async def ping(ctx):
 @bot.command()
 async def post(ctx):
     z = ctx.message.content
-    embedVar = discord.Embed(description=z[6:100000000000], color=0x00ff00)
+    embedVar = discord.Embed(description=z[6:100000000000], color=0xe6e6fa)
     if len(ctx.message.attachments) > 0:
         for sex in range(len(ctx.message.attachments)):
             y = ctx.message.attachments[sex].url
@@ -130,6 +147,8 @@ async def post(ctx):
     else:
         await ctx.send(embed=embedVar)
         await ctx.message.delete()
+
+
 # Add realization more than 1 pic
 # Guilds
 
